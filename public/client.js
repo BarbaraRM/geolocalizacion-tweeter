31ea858a5de4981ea430df43485ea7b3108d3918
BarbaraRM <70588575+BarbaraRM@users.noreply.github.com>
2020-09-11 12:38:29 -0500

    Update client.js

--- public/client.js
+++ public/client.js
@@ -8,20 +8,20 @@ const outputMessage = document.getElementById('respuesta');
 const texto = document.getElementById('texto');
 
 buttonSend.addEventListener('click', function(e){ 
-    if(user.value === ""){
-        texto.value = "Ingrese una cuenta correcta"
+    if(user.value === ''){
+        texto.innerHTML  = 'Ingrese una cuenta correcta'
     }else{
         socket.emit('pais', user.value.trim().toLowerCase().replace(" ",""))
+        texto.innerHTML  = ''
     }
 });
 
 socket.on('localizacion',data=>{
     
-    if(data === "El usuario no tiene registrado su Ubicacion"){
-        texto.value= `${data}`;
+    if(data === 'El usuario no tiene registrado su Ubicacion'){
+        texto.innerHTML = `${data}`;
     }else{
         outputMessage.value= `${data}`;
-    }
-   
-
+        texto.innerHTML  = ''
+    }  
 });
